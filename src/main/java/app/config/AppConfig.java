5402f110af2d3a602bc3b2f6e985493c1e3ce601
hkl <hkl@local>
2023-01-24 15:50:06 +0500

    test passed

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,15 +1,15 @@
 package app.config;
 
-import app.model.Dog;
-import org.springframework.context.annotation.Bean;
+/*import app.model.Dog;
+import org.springframework.context.annotation.Bean;*/
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
-    @Bean(name="dog")
+/*    @Bean(name="dog")
     public Dog getDog() {
         return new Dog();
-    }
+    }*/
 }
